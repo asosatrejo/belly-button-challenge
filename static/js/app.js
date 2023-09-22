8ce59dc4f4c910bc147ca381190b7602d22340eb
ariana <a.sostrejo75@gmail.com>
2023-09-21 23:54:18 -0400

    bug fixed

--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,5 @@
 // Use the D3 library to read in samples.json from the URL
-var url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
+let url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
 
 d3.json(url).then((data) => {console.log(data)});
 
@@ -7,15 +7,15 @@ d3.json(url).then((data) => {console.log(data)});
 function buildCharts(sample){
     d3.json(url).then((data) => {
 
-        var value = data.samples.filter(result => result.id === sample);
-        var valueData = value[0];
+        let value = data.samples.filter(result => result.id == sample);
+        let valueData = value[0];
 
-        var sample_values = valueData.sample_values;
-        var otu_ids = valueData.otu_ids;
-        var otu_labels = valueData.otu_labels;
+        let sample_values = valueData.sample_values;
+        let otu_ids = valueData.otu_ids;
+        let otu_labels = valueData.otu_labels;
 
         // Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
-        var bar_data = [{
+        let bar_data = [{
             // Use `sample_values` as the values for the bar chart.
             x: sample_values.slice(0, 10).reverse(),
             // Use `otu_ids` as the labels for the bar chart.
@@ -25,14 +25,14 @@ function buildCharts(sample){
             type: "bar"}
         ];
 
-        var layout = {
+        let layout = {
             title: "Top 10 OTUs Found in Individual"
         };
 
         Plotly.newPlot("bar", bar_data, layout)
 
         // Create a bubble chart that displays each sample.
-        var bubble_data = [{
+        let bubble_data = [{
             // Use `otu_ids` for the x values.
             x: otu_ids,
             // Use `sample_values` for the y values.
@@ -54,8 +54,8 @@ function buildCharts(sample){
 // Display each key-value pair from the metadata JSON object on the page.
 function buildMetadata(sample){
     d3.json(url).then((data) => {
-        var value = data.metadata.filter(result => result.id === sample);
-        var valueData = value[0];
+        let value = data.metadata.filter(result => result.id == sample);
+        let valueData = value[0];
 
         d3.select("#sample-metadata").html("");
 
@@ -67,7 +67,7 @@ function buildMetadata(sample){
 
 // Display the sample metadata, i.e., an individual's demographic information.
 function init(){
-    var dropdown = d3.select("#selDataset");
+    let dropdown = d3.select("#selDataset");
 
     // Use Sample Names for Select Options
     d3.json(url).then((data) => {
@@ -78,7 +78,7 @@ function init(){
         
         
         // Build First Plots
-        var firstSample = names[0];
+        let firstSample = names[0];
         buildCharts(firstSample);
         buildMetadata(firstSample);
     });
